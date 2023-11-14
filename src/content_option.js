cea42e5420ba9fa135dd19126d5ad19470546655
Manju-Sunil <143173811+Manju-Sunil@users.noreply.github.com>
2023-11-14 15:51:44 +0530

    Add files via upload

---
+++ src/content_option.js
@@ -0,0 +1,142 @@
+
+// import manju from './Porfolio (1).png';
+
+const logotext = "MANJU.T";
+const meta = {
+    title: "Manju",
+    description: "I’m Manju Frontend developer _ WebPage Designer,currently searching for job",
+};
+
+const introdata = {
+    title: "I’m MANJU",
+    animated: {
+        first:"Fresher", 
+        second: "Frontend developer",
+        third:  "WebPage Designer",
+    },
+    description: "As a Fresher, I work as a front-end developer and web designer.",
+                      
+    your_img_url:'https://media.licdn.com/dms/image/D4E03AQFBx9CejgtuQg/profile-displayphoto-shrink_800_800/0/1699943218932?e=1705536000&v=beta&t=XqedtleEoGNbyd757vvW0cIKEinpHjsxsiO8K938MqA',
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "Hey, I'm Manju,  I have to say something about myself.My strengths are being a quick learner and positive thinking with moves those all around me forward...",
+};
+const worktimeline = [{
+        jobtitle: "Designer of week",
+        where: "YAdfi",
+        date: "2020",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "Jamalya",
+        date: "2019",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "ALquds",
+        date: "2019",
+    },
+];
+
+const skills = [{
+        name: "HTML",
+        value: 90,
+    },
+    {
+        name: "CSS",
+        value: 90,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 90,
+    },
+    {
+        name: "Bootstrap",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "Webpage Design",
+        description: "Web designing is the process of planning, conceptualizing, and implementing the plan for designing a website in a way that is functional and offers a good user experience.",
+    },
+    {
+        title: "Shopping Cart",
+        description: " A shopping cart on an online retailer's site is a piece of software that facilitates the purchase of a product or service. It accepts the customer's payment and organizes the distribution of that information to the merchant, payment processor and other parties..",
+    },
+    {
+        title: "Registration form with Validation",
+        description: "A Form Validation is a technical process where a web form checks if the information provided by a user is correct. The form will either alert the user that they missed something and need to fix something to proceed, or the form will be validated and the user will be able to continue with their registration process..",
+    },
+];
+
+const dataportfolio = [{
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4LYJSrJzQwtGJI5VjuBH20xJsa0C8vneV67OvM9ETSGVsA6fDFQNbVepqD5bi_fIRv_E&usqp=CAU',
+        description: "To Create Shopping Cart WebPage.",
+        link: "#",
+    },
+    {
+        img: "https://webdevtrick.com/wp-content/uploads/registration-form-with-validation.jpg",
+        description: "Registration Form With Validation.",
+        link: "#",
+    },
+    {
+        img: "https://blog.magezon.com/wp-content/uploads/2022/03/image-32-1536x789.png",
+        description: "To Create Webpage.",
+        link: "#",
+    },
+    {
+        img: "https://hrycan.files.wordpress.com/2012/03/table1.jpg",
+        description: "Lazy Loading.",
+        link: "#",
+    },
+    {
+        img: "https://codersera.com/blog/wp-content/uploads/2019/12/how-to-use-api-call.jpg",
+        description: "API Fetching.",
+        link: "#",
+    },
+    {
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSp6Qc3Els6SQRPPS3Y3GByrvJn6mxfPYli3w&usqp=CAU",
+        description: "Authentication Page.",
+        link: "#",
+    },
+
+
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "tmanju35@gmail.com",
+    YOUR_FONE: "+91-7708761956",
+    description: "Believe in yourself. You are braver than you think,more talented than you know, and capable of more than you imagine. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://www.facebook.com/manju.thankaraj?mibextid=ZbWKwL",
+    linkedin: "www.linkedin.com/in/manju-t-b8262821b",
+    
+    instagram: "https://instagram.com/manjusunil_t?igshid=MTk0NTkyODZkYg==",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
